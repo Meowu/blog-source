144261b74824816e9ad5aaea308ce8f79f42639b
meowu <474384902@qq.com>
2019-12-24 23:54:50 +0800

    pass cors preflighted.

--- src/cors/server3000.js
+++ src/cors/server3000.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
-
-app.get('/greet', (req, res) => {
+const WHITE_LIST = ['http://127.0.0.1:4000']
+app.use((req, res, next) => {
+    const { origin } = req.headers;
     console.log('headers', req.headers);
+    if (WHITE_LIST.includes(origin)) {
+        res.set({
+            'Access-Control-Allow-Origin': origin,
+            'Access-Control-Allow-Methods': 'GET'
+        })
+    }
+    next();
+})
+app.get('/greet', (req, res) => {
+    console.log('headers', req.header);
     res.send('Hello CORS.')
 })
 app.listen(3000);
